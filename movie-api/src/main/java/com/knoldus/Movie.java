0b87057bd990f88de9d732553726f698a7136828
abhisheknoldus <abhishek@knoldus.com>
2017-02-26 10:33:49 +0530

    annotation imported

--- movie-api/src/main/java/com/knoldus/Movie.java
+++ movie-api/src/main/java/com/knoldus/Movie.java
@@ -1,6 +1,9 @@
 package com.knoldus;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
 
 import javax.annotation.concurrent.Immutable;
 
